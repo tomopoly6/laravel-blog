c5b5b5cc76b5dd357903d6cbe31804a4d4abd3a0
tomohito <tomo@MacBook-Air-2012.local>
2018-11-29 00:20:14 +0900

    add pagination

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class ArticleController extends Controller
 {
     public function index(){
-        $articles = Article::all();
+
+        $articles = Article::latest()->paginate(10);
         return view('article.index',['articles' => $articles]);
     }
 
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -20,6 +20,8 @@
       </div>
     </div>
     @endforeach
+
+    {{ $articles->links() }}
     <p><a href="/create" class="btn btn-primary">新規追加</a></p>
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
