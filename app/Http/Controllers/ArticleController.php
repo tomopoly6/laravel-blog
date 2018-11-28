9c58dc9c8f0f86cb7111f5039417dcea1c402bc6
tomohito <tomo@MacBook-Air-2012.local>
2018-11-28 23:28:13 +0900

    first commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    public function index(){
+        $articles = Article::all();
+        return view('article.index',['articles' => $articles]);
+    }
+
+    public function create() {
+        return view('article.create');
+    }
+
+    public function store(Request $request){
+        $article = new Article;
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->save();
+
+        return view('article.store');
+    }
+
+    public function edit(Request $request, $id) {
+        $article = Article::find($id);
+        return view('article.edit', ['article' => $article]);
+    }
+
+    public function update(Request $request){
+        $article = Article::find($request->id);
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->save();
+
+        return view('article.update');
+    }
+
+    public function show(Request $request, $id){
+        $article = Article::find($id);
+        return view('article.show', ['article' => $article]);
+    }
+
+    public function delete(Request $request){
+        Article::destroy($request->id);
+        return view('article.delete');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'ArticleController@index');
+
+//記事の作成
+Route::get('create', 'ArticleController@create');
+Route::post('create', 'ArticleController@store');
+
+//記事の編集
+//引数を{id}として渡す
+Route::get('edit/{id}', 'ArticleController@edit');
+Route::post('edit', 'ArticleController@update');
+
+//記事の削除
+Route::get('delete/{id}', 'ArticleController@show');
+Route::post('delete', 'ArticleController@delete');
