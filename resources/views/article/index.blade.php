25bf188f5ea2da6a9698a3075f707f0a812c8429
tomohito <tomo@MacBook-Air-2012.local>
2018-12-02 21:42:58 +0900

    before validation

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -11,14 +11,19 @@
         <h4 class="card-title">{{ $article->title }}</h4>
         <h6 class="card-subtitle mb-2 text-muted">{{ $article->updated_at }}</h6>
         <p class="card-text">{{ $article->body }}</p>
+        @if(Auth::check())
         <a href="/edit/{{ $article->id }}" class="card-link">修正</a>
         <a href="/delete/{{ $article->id }}" class="card-link">削除</a>
+        @endif
       </div>
     </div>
     @endforeach
 
     {{ $articles->links() }}
-    <p><a href="/create" class="btn btn-primary">新規追加</a></p>
+    <p>
+    <a href="/create" class="btn btn-primary">新規追加</a>
+    <a href="/login" class="btn btn-info">管理者ログイン</a>
+    </p>
 
     
 
